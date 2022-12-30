0ee7fea3a374dab537c0f01fa9d3dccf2876e0b2
James Almond <james.almond@cinch.co.uk>
2022-12-30 11:23:20 +0000

    feat: add pageId to main element

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,19 @@ import "../styles/globals.css"
 import { Header } from "../components/header/header.component"
 import { Footer } from "../components/footer/footer.component"
 
+import { useRouter } from "next/router"
+
 import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
+    const router = useRouter()
+    const pageId = router.pathname.substring(1)
+
     return (
         <>
             <Header />
 
-            <main>
+            <main id={pageId}>
                 <Component {...pageProps} />
             </main>
 
