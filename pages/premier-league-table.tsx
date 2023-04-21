cd40066be5e25f4d9fff76465cbad586bb482d70
James Almond <james.almond@cinch.co.uk>
2023-04-21 16:07:00 +0100

    feat: add form column (in progress)

--- pages/premier-league-table.tsx
+++ pages/premier-league-table.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import cx from "classnames"
 
 import { fetchData } from "../services/api"
 
@@ -40,24 +41,41 @@ const PremierLeagueTable = () => {
 							<th scope="col">Won</th>
 							<th scope="col">Drawn</th>
 							<th scope="col">Lost</th>
+							<th scope="col">Form</th>
 						</tr>
 					</thead>
 					<tbody>
 						{data?.standings &&
 							data.standings[0].table.map((position, index) => {
 								const tablePosition = index + 1
+								const gameForm = position.form.split(",").reverse()
 
 								return (
 									<tr key={`${position.team.id}`}>
-										<td key={`${position.team.shortName}-${index}`}>{tablePosition}</td>
-										<td key={`${position.team.shortName}`}>{position.team.shortName}</td>
-										<td key={`${position.team.shortName}-${position.playedGames}-playedGames`}>
-											{position.playedGames}
+										<td key={`${position.team.id}-${index}`}>{tablePosition}</td>
+										<td key={`${position.team.id}`}>{position.team.shortName}</td>
+										<td key={`${position.team.id}-${position.playedGames}-playedGames`}>{position.playedGames}</td>
+										<td key={`${position.team.id}-${position.points}-points`}>{position.points}</td>
+										<td key={`${position.team.id}-${position.won}-won`}>{position.won}</td>
+										<td key={`${position.team.id}-${position.draw}-draw`}>{position.draw}</td>
+										<td key={`${position.team.id}-${position.lost}-lost`}>{position.lost}</td>
+										<td key={`${position.team.id}-${position.form}-form`}>
+											<div className={styles["game-result"]}>
+												{gameForm.map((gameResult, index) => {
+													const gameResultPillClassName = `game-result-pill-${gameResult}`
+													console.log(gameResultPillClassName)
+
+													return (
+														<div
+															className={cx(styles["game-result-pill"], styles[`${gameResultPillClassName}`])}
+															key={`${gameResult}-${index}`}
+														>
+															{gameResult}
+														</div>
+													)
+												})}
+											</div>
 										</td>
-										<td key={`${position.team.shortName}-${position.points}-points`}>{position.points}</td>
-										<td key={`${position.team.shortName}-${position.won}-won`}>{position.won}</td>
-										<td key={`${position.team.shortName}-${position.draw}-draw`}>{position.draw}</td>
-										<td key={`${position.team.shortName}-${position.lost}-lost`}>{position.lost}</td>
 									</tr>
 								)
 							})}
