9ea56140dea4f9af8e0c3f00687d13f046657d74
James Almond <james.almond@cinch.co.uk>
2023-04-25 14:09:47 +0100

    feat: add responsive react elements

--- components/premier-league-table/premier-league-table.tsx
+++ components/premier-league-table/premier-league-table.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { useMediaQuery } from "react-responsive"
 import cx from "classnames"
 
 import { fetchData } from "../../services/api"
@@ -6,46 +7,49 @@ import { fetchData } from "../../services/api"
 import styles from "./premier-league-table.module.scss"
 
 interface PremierLeagueData {
-	competition: {
-		name: string
-	}
-	standings: [
-		{
-			table
-		}
-	]
+	competition
+	standings
 }
 
 export const PremierLeagueTable = () => {
 	const [data, setData] = useState<PremierLeagueData>({} as PremierLeagueData)
 
+	const isMediumOrAbove = useMediaQuery({
+		query: "(min-width: 600px)",
+	})
+	const isLargeOrAbove = useMediaQuery({
+		query: "(min-width: 1100px)",
+	})
+
 	useEffect(() => {
 		fetchData(setData)
 	}, [])
 
-	console.clear()
 	console.log(data)
 
 	return (
 		<>
-			<h1>{data?.competition?.name} table</h1>
+			<h1>
+				{data.competition?.name}
+				<img src={data.competition?.emblem} alt={`${data.competition?.name} crest.`} />
+			</h1>
 
 			<section className={styles["premier-league-table-wrapper"]}>
 				<table className={styles["premier-league-table"]}>
 					<thead>
 						<tr>
-							<th scope="col">Position</th>
-							<th scope="col">Team</th>
-							<th scope="col">Played</th>
-							<th scope="col">Points</th>
-							<th scope="col">Won</th>
-							<th scope="col">Drawn</th>
-							<th scope="col">Lost</th>
+							<th scope="col">{isMediumOrAbove ? "Position" : "Pos"}</th>
+							<th scope="col">Club</th>
+							<th scope="col">{isMediumOrAbove ? "Played" : "Pld"}</th>
+							<th scope="col">{isMediumOrAbove ? "Won" : "W"}</th>
+							<th scope="col">{isMediumOrAbove ? "Drawn" : "D"}</th>
+							<th scope="col">{isMediumOrAbove ? "Lost" : "L"}</th>
+							<th scope="col">{isMediumOrAbove ? "Points" : "Pts"}</th>
 							<th scope="col">Form</th>
 						</tr>
 					</thead>
 					<tbody>
-						{data?.standings &&
+						{data.standings &&
 							data.standings[0].table.map((position, index) => {
 								const tablePosition = position.position
 								const teamName = position.team.name.replace(" FC", "")
@@ -57,14 +61,14 @@ export const PremierLeagueTable = () => {
 										<td key={`${position.team.id}`}>
 											<div className={styles["team"]}>
 												<img src={position.team.crest} alt={`${teamName} crest.`} className={styles["team-crest"]} />
-												{teamName}
+												{isLargeOrAbove ? teamName : position.team.tla}
 											</div>
 										</td>
 										<td key={`${position.team.id}-${position.playedGames}-playedGames`}>{position.playedGames}</td>
-										<td key={`${position.team.id}-${position.points}-points`}>{position.points}</td>
 										<td key={`${position.team.id}-${position.won}-won`}>{position.won}</td>
 										<td key={`${position.team.id}-${position.draw}-draw`}>{position.draw}</td>
 										<td key={`${position.team.id}-${position.lost}-lost`}>{position.lost}</td>
+										<td key={`${position.team.id}-${position.points}-points`}>{position.points}</td>
 										<td key={`${position.team.id}-${position.form}-form`}>
 											<div className={styles["game-result"]}>
 												{gameForm.map((gameResult, index) => {
