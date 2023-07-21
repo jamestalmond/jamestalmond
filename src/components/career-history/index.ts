0d829ce5468426d69fdab69eb05f9e7226c29aa8
James Almond <james.almond@cinch.co.uk>
2023-07-21 13:36:41 +0100

    refactor: move career history sections into separate component

---
+++ src/components/career-history/index.ts
@@ -0,0 +1,1 @@
+export { CareerHistory } from './career-history.component'
--- src/pages/career-history.tsx
+++ src/pages/career-history.tsx
@@ -1,51 +1,13 @@
-import styles from "../styles/career-history.module.scss"
+import { CareerHistory } from "../components/career-history/career-history.component"
 
-export const CareerHistory = () => {
+const CareerHistoryPage = () => {
 	return (
 		<>
 			<h1>Career History</h1>
 
-			<section className={styles["career-history-item"]}>
-				<h2>cinch</h2>
-				<p className={styles["career-history-item-subtitle"]}>Lorem ipsum dolor.</p>
-				<p>Lorem ipsum dolor.</p>
-			</section>
-
-			<section className={styles["career-history-item"]}>
-				<h2>Auto Trader</h2>
-				<p className={styles["career-history-item-subtitle"]}>
-					Auto Trader is the UK and Ireland’s largest digital automotive marketplace. It has tech offices based in
-					Manchester and London and employs over c.750 people.
-				</p>
-				<p>Lorem ipsum dolor.</p>
-			</section>
-
-			<section className={styles["career-history-item"]}>
-				<h2>AO</h2>
-				<p className={styles["career-history-item-subtitle"]}>
-					AO is a £1bn t/o FTSE 250 listed business, specialising in the online sale and delivery of electricals. The
-					tech department spans 2 sites and is made up of c. 200 people.
-				</p>
-				<p>Lorem ipsum dolor.</p>
-			</section>
-
-			<section className={styles["career-history-item"]}>
-				<h2>ekm</h2>
-				<p className={styles["career-history-item-subtitle"]}>
-					ekm is a CMS platform that allows businesses to create online ecommerce shops using templates, c. 80,000 shops
-					created.
-				</p>
-				<p>
-					I started at ekm as an ecommerce executive helping shop owners set up their businesses online as fast as
-					possible.
-				</p>
-				<p>
-					During this employment I learned how to develop websites off my own back in my own time. I was promoted to
-					design and build the shop templates myself.
-				</p>
-			</section>
+			<CareerHistory />
 		</>
 	)
 }
 
-export default CareerHistory
+export default CareerHistoryPage
