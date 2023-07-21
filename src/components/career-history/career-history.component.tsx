5fe692981775f812cb74ad6a5d80ee126efb05b5
James Almond <james.almond@cinch.co.uk>
2023-07-21 14:34:36 +0100

    feat: add career history content

--- src/components/career-history/career-history.component.tsx
+++ src/components/career-history/career-history.component.tsx
@@ -4,34 +4,167 @@ export const CareerHistory = () => {
 	return (
 		<>
 			<section className={styles["career-history-item"]}>
-				<h2>cinch</h2>
+				<h2>
+					<a href="https://cinch.co.uk" target="_blank" rel="nofollow">
+						cinch
+					</a>
+				</h2>
 				<p className={styles["career-history-item-subtitle"]}>
 					cinch is a retailer specialising in taking the faff out of the online car buying and ownership journey. It has
 					tech offices in Manchester and London and employs over c.400 people.
 				</p>
-				<p>Lorem ipsum dolor.</p>
+				<h3>Senior Software Engineer</h3>
+				<p>
+					Worked in the Design System Squad as a Senior Software Engineer focusing on the front end. Responsibilities
+					included:
+				</p>
+				<ul>
+					<li>Architecting, designing, and building a brand new in-house design system for cinch.</li>
+					<li>
+						Working with various product teams within the business to ensure our design system was fit for purpose,
+						taking their feedback into consideration to help drive the design system forward.
+					</li>
+					<li>Breaking down new features and concepts into workable tickets.</li>
+					<li>Upskilling members of the squad during our daily pairing and mobbing sessions.</li>
+				</ul>
+				<p>
+					After a wave of redundancies, the Design System Squad was disbanded so I joined a product team focusing on
+					acquiring customers and engaging with them. Responsibilities include:
+				</p>
+				<ul>
+					<li>Working with the squad to architect, design, build, and maintain landing pages for our estate.</li>
+					<li>Running refinement sessions with the squad to break down our work into tickets.</li>
+					<li>Upskilling members of the squad during our daily pairing and mobbing sessions.</li>
+				</ul>
+				<p>Other notable accomplishments:</p>
+				<ul>
+					<li>Wrote various internal blog posts on technical and non-technical subjects.</li>
+					<li>Demoed Design System releases and product work releases.</li>
+					<li>Organised social events with the squad outside of work.</li>
+				</ul>
 			</section>
 
 			<section className={styles["career-history-item"]}>
-				<h2>Auto Trader</h2>
+				<h2>
+					<a href="https://autotrader.co.uk" target="_blank" rel="nofollow">
+						Auto Trader UK
+					</a>
+				</h2>
 				<p className={styles["career-history-item-subtitle"]}>
 					Auto Trader is the UK and Ireland’s largest digital automotive marketplace. It has tech offices based in
 					Manchester and London and employs over c.750 people.
 				</p>
-				<p>Lorem ipsum dolor.</p>
+				<h3>Senior Front End Developer</h3>
+				<p>
+					Worked as a member of the Browsing and Research squad as a Senior Front End Developer. Responsibilities
+					included:
+				</p>
+				<ul>
+					<li>
+						Constructing a variety of SPAs used on Auto Trader made up of React, TypeScript, GraphQL, styled-components,
+						and our in-house design system.
+					</li>
+					<li>
+						Architecting, designing, and upkeeping the design system used at Auto Trader working alongside designers and
+						developers.
+					</li>
+					<li>
+						Breaking down high-level concepts into workable tickets (user stories/tasks) in order to deliver value to
+						the user faster. This was usually performed in conjunction with the input of stakeholders, designers,
+						developers, and testers.
+					</li>
+					<li>
+						Mentoring an apprentice developer through the beginning of their career. We paired frequently and had
+						regular 1:1 catch-ups to assess progress and goals.
+					</li>
+				</ul>
+				<p>Other notable accomplishments:</p>
+				<ul>
+					<li>Replatforming Auto Trader’s editorial content space to a responsive React SPA.</li>
+					<li>
+						Leading the development of new brand pages that allow consumers to find their perfect car. These pages are
+						constructed using React, TypeScript, GraphQL, styled-components, and our design system components.
+					</li>
+					<li>
+						Completing a Carbon Literacy course. This is a recognised certificate that shows I am aware of the impact of
+						climate change and the effect I and others have on the environment.
+					</li>
+					<li>
+						Starting at Auto Trader fully remote during the pandemic and integrated myself into the squad to become a
+						trustworthy and dependable member of the team.
+					</li>
+					<li>Rewarded a promotion to Senior Front End Developer as a result of my hard work and dedication.</li>
+					<li>
+						Volunteering with the squad to help the staff of a local park clean up the area and provide any other
+						assistance they required.
+					</li>
+				</ul>
 			</section>
 
 			<section className={styles["career-history-item"]}>
-				<h2>AO</h2>
+				<h2>
+					<a href="https://ao.com" target="_blank" rel="nofollow">
+						AO
+					</a>
+				</h2>
 				<p className={styles["career-history-item-subtitle"]}>
 					AO is a £1bn t/o FTSE 250 listed business, specialising in the online sale and delivery of electricals. The
 					tech department spans 2 sites and is made up of c. 200 people.
 				</p>
-				<p>Lorem ipsum dolor.</p>
+				<h3>Front End Developer</h3>
+				<p>Worked as a member of the product lister squad as a Front End Developer. Responsibilities included:</p>
+				<ul>
+					<li>
+						Architecting the product and its features to provide continuous improvements and value for the business,
+						such as adding a responsive slide-out drawer to display content to users to upsell various features on new
+						products.
+					</li>
+					<li>Pairing with and upskilling various team members including developers and QA testers.</li>
+					<li>
+						Working closely with the Product Owner and Principle Developer to improve our technical estate to prepare
+						for any requirements. Regular meetings were held in which opportunities for technical improvements were
+						raised and prioritised.
+					</li>
+					<li>
+						Creating various A/B experiments to improve the product over time based on real user data. One change in
+						particular updated the design of the product to improve the UX of the page; this was a success based on
+						various metrics such as click-through rate, bounce rate reduction, and add to basket rate.
+					</li>
+					<li>Ensuring the product was fully tested and met accessibility standards.</li>
+				</ul>
+				<p>Other notable accomplishments:</p>
+				<ul>
+					<li>
+						Worked in the team during its transition to a product team (squad) and provided technical leadership on a
+						project to re-platform the entire product lister application to a responsive React SPA in order to pave the
+						way for the squad to exist and become autonomous.
+					</li>
+					<li>
+						Led a team that created an internal CSS framework. This framework was constructed using SCSS based on ITCSS
+						architecture and allows developers to rapidly create consistent UI across products.
+					</li>
+					<li>
+						Worked on the product page team helping create a React application that provided attachable products to a
+						master product, giving customers more options when they purchased a product. This provided huge value to the
+						business.
+					</li>
+					<li>
+						Led workshops on CSS Flexbox and Grid for the front end team, upskilling the entire team on the latest
+						layout methods on the web.
+					</li>
+					<li>
+						Mentored apprentices coming into the front end team, giving them the tools and experience they needed to
+						succeed.
+					</li>
+				</ul>
 			</section>
 
 			<section className={styles["career-history-item"]}>
-				<h2>ekm</h2>
+				<h2>
+					<a href="https://ekm.com" target="_blank" rel="nofollow">
+						ekm
+					</a>
+				</h2>
 				<p className={styles["career-history-item-subtitle"]}>
 					ekm is a CMS platform that allows businesses to create online ecommerce shops using templates, c. 80,000 shops
 					created.
